0099a388326eb88e02ffdc4c4ba91b6e48608a1f
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-05-21 09:46:14 -0300

    Tecnico tables

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA: Tecnico[] = [{
+    id:1,
+    nome:'Thiago Martins',
+    cpf:'123.456.789-10',
+    email:'thiago@mail.com',
+    senha:'1234',
+    perfis:['0'],
+    dataCriacao: '15/08/2023'
+  }]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'opcoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
+
