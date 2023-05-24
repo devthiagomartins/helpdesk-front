a57af2816c5f8956eac76ccf5735d65059eb9ece
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-05-23 21:38:34 -0300

    html css Tecnico Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
@@ -15,6 +16,9 @@ const routes: Routes = [
   },
   {
     path:'tecnicos', component: TecnicoListComponent
+  },
+  {
+    path:'tecnicos/create', component: TecnicoCreateComponent
   }
 ]
 
